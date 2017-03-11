78d6e20b1a30e04d72ec3024365ee344b94eff9b
GhAbd <ghazal.abdalrahman@gmail.com>
2017-03-11 08:42:29 +0200

    Write the method filterByDistanceFrom

--- EarthQuakeClient.java
+++ EarthQuakeClient.java
@@ -23,7 +23,11 @@ public class EarthQuakeClient {
     Location from) {
         ArrayList<QuakeEntry> answer = new ArrayList<QuakeEntry>();
         // TODO
-
+        for(QuakeEntry qe : quakeData ){
+            if (from.distanceTo(qe.getLocation())<distMax){
+                answer.add(qe);
+            }
+        }
         return answer;
     }
 
