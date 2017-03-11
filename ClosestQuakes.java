7affd9150a2b61d60e510e5299a0ace5230e2fde
GhAbd <ghazal.abdalrahman@gmail.com>
2017-03-11 11:11:18 +0200

    Find the Closest Earthquakes Class

--- ClosestQuakes.java
+++ ClosestQuakes.java
@@ -11,8 +11,21 @@ import java.util.*;
 public class ClosestQuakes {
     public ArrayList<QuakeEntry> getClosest(ArrayList<QuakeEntry> quakeData, Location current, int howMany) {
         ArrayList<QuakeEntry> ret = new ArrayList<QuakeEntry>();
+        ArrayList<QuakeEntry> cData = new ArrayList<QuakeEntry>(quakeData);
         // TO DO
-
+        for (int i = 0;i<howMany;i++){
+            QuakeEntry closestQE = cData.get(0);
+            for(QuakeEntry qe : cData){
+                if (current.distanceTo(qe.getLocation())< current.distanceTo(closestQE.getLocation()) ){
+                    closestQE = qe;
+                }
+            }
+            ret.add(closestQE);
+            cData.remove(closestQE);
+            if (cData.size()==0){
+                break;
+            }
+        }
         return ret;
     }
 
@@ -25,7 +38,7 @@ public class ClosestQuakes {
 
         Location jakarta  = new Location(-6.211,106.845);
 
-        ArrayList<QuakeEntry> close = getClosest(list,jakarta,10);
+        ArrayList<QuakeEntry> close = getClosest(list,jakarta,3);
         for(int k=0; k < close.size(); k++){
             QuakeEntry entry = close.get(k);
             double distanceInMeters = jakarta.distanceTo(entry.getLocation());
